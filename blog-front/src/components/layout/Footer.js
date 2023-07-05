0f2517d80d536709ead2aa32bb5c2afacc55bcb4
Xariif <59657029+Xariif@users.noreply.github.com>
2023-07-05 13:37:50 +0200

    gitignore update

--- blog-front/src/components/layout/Footer.js
+++ blog-front/src/components/layout/Footer.js
@@ -1,13 +1,8 @@
 export default function Footer() {
-
-
-  //wykonawcy i dane dot projektu
-  // ikonki do social mediów
-  // nazwa firmy + znak zastrzezony + rok (aktualny z js)
-
-  return (
-    <div className="footer">
-      footer
-    </div>
-  );
-}
+    //wykonawcy i dane dot projektu
+    // ikonki do social mediów
+    // nazwa firmy + znak zastrzezony + rok (aktualny z js)
+  
+    return <div className="footer">footer</div>;
+  }
+  
