c5005f1b8418a1fb8801aace7eefbce8daad091f
Jakub Filiks <jakubfiliks7@gmail.com>
2023-11-08 23:05:51 +0100

    fixes

--- blog-front/src/index.js
+++ blog-front/src/index.js
@@ -6,27 +6,31 @@ import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { ReducerProvider } from "./context/ReducerContext";
 
+import { Provider } from "react-redux";
+
 const rootElement = document.getElementById("root");
 
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
 	<React.StrictMode>
-		<Auth0Provider
-			domain="dev-uasjfxeuwrj58j4g.us.auth0.com"
-			clientId="zxa0jWBh7donY0oLbt3X4FCkWVQQ9yc7"
-			authorizationParams={{
-				redirect_uri: window.location.origin,
-				audience: "https://b-tech-api",
-				scope: "openid profile email admin author user",
-			}}
-			cacheLocation="localstorage"
-		>
-			<BrowserRouter>
-				<ReducerProvider>
-					<App />
-				</ReducerProvider>
-			</BrowserRouter>
-		</Auth0Provider>
+		<Provider store={store}>
+			<Auth0Provider
+				domain="dev-uasjfxeuwrj58j4g.us.auth0.com"
+				clientId="zxa0jWBh7donY0oLbt3X4FCkWVQQ9yc7"
+				authorizationParams={{
+					redirect_uri: window.location.origin,
+					audience: "https://b-tech-api",
+					scope: "openid profile email admin author user",
+				}}
+				cacheLocation="localstorage"
+			>
+				<BrowserRouter>
+					<ReducerProvider>
+						<App />
+					</ReducerProvider>
+				</BrowserRouter>
+			</Auth0Provider>
+		</Provider>
 	</React.StrictMode>
 );
